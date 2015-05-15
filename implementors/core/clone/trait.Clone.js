58c25b71c7662a15409fa57b1e35ba658f3bad5f
Jorge Aparicio <japaricious@gmail.com>
2015-05-15 13:51:38 +0000

    docs!

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['simd'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='simd/struct.f32x4.html' title='simd::f32x4'>f32x4</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='simd/struct.f64x2.html' title='simd::f64x2'>f64x2</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
